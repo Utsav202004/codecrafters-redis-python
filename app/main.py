71aa4e68987d1ac762a2023ed4ad5ddf4070dc79
Utsav Goyal <goyalutsav2004@gmail.com>
2025-10-22 16:20:56 +0530

    Handling multiple PING requests: fix 2: hardcoding PONG multiple responses

--- app/main.py
+++ app/main.py
@@ -3,12 +3,10 @@ import socket  # noqa: F401
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, address = server_socket.accept() # wait for client
+    conn, address = server_socket.accept() # wait for client
 
-    while connection.recv(1024):
-        data = connection.recv(1024)
-        if data == b"*1\r\n$4\r\nPING\r\n":
-            connection.sendall(b"+PONG\r\n")
+    while conn.recv(1024):
+        conn.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
