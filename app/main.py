49c875997b81856df9e1b6bd347780f5c1d105f0
Utsav Goyal <goyalutsav2004@gmail.com>
2025-10-22 16:19:50 +0530

    Handling multiple PING requests: fix 1

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
 
-    while connection.recv():
+    while connection.recv(1024):
         data = connection.recv(1024)
         if data == b"*1\r\n$4\r\nPING\r\n":
             connection.sendall(b"+PONG\r\n")
