d8f08e78e49bf3c4950145a5f19a0111d2824d1b
Utsav Goyal <goyalutsav2004@gmail.com>
2025-10-22 16:41:28 +0530

    Handling multiple PING requests: fix 3: carefully using the conn.recv function

--- app/main.py
+++ app/main.py
@@ -5,11 +5,16 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, address = server_socket.accept() # wait for client
 
-    while conn.recv(1024):
+    while True:
         data = conn.recv(1024)
-        if data == b"*1\r\n$4\r\nping\r\n":
+        
+        if not data:
+            break
+
+        if data == b"*1\r\n$4\r\nPING\r\n":
             conn.sendall(b"+PONG\r\n")
 
+    conn.close()
 
 if __name__ == "__main__":
     main()
